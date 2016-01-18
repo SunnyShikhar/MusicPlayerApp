92a316d058f9eed54db0cf6c3510b1939824c841
sunnyshikhar <shikhar.gaur.13@gmail.com>
2016-01-18 02:02:33 -0500

    Song playback functionality implemented, even when device is locked.

--- app/src/main/java/com/example/sunnyshikhar/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/sunnyshikhar/musicplayer/MainActivity.java
@@ -12,12 +12,24 @@ import android.net.Uri;
 import android.content.ContentResolver;
 import android.database.Cursor;
 import android.widget.ListView;
+import android.os.IBinder;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.view.MenuItem;
+import android.view.View;
+
 
 
 public class MainActivity extends ActionBarActivity {
 
     private ArrayList<Song> songList;
     private ListView songView;
+
+    private MusicService musicSrv;
+    private Intent playIntent;
+    private boolean musicBound=false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,26 +53,47 @@ public class MainActivity extends ActionBarActivity {
         songView.setAdapter(songAdt);
     }
 
+    //connect to the service
+    private ServiceConnection musicConnection = new ServiceConnection(){
+
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            MusicService.MusicBinder binder = (MusicService.MusicBinder)service;
+            //get service
+            musicSrv = binder.getService();
+            //pass list
+            musicSrv.setList(songList);
+            musicBound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            musicBound = false;
+        }
+    };
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-//        // Handle action bar item clicks here. The action bar will
-//        // automatically handle clicks on the Home/Up button, so long
-//        // as you specify a parent activity in AndroidManifest.xml.
-//        int id = item.getItemId();
-//
-//        //noinspection SimplifiableIfStatement
-//        if (id == R.id.action_settings) {
-//            return true;
-//        }
 
+        switch(item.getItemId()){
+
+            case R.id.action_end:
+                //close the app
+                //stopService(play)
+                stopService(playIntent);
+                musicSrv = null;
+                System.exit(0);
+                break;
+
+            case R.id.action_shuffle:
+                break;
+        }
         return super.onOptionsItemSelected(item);
     }
 
@@ -92,4 +125,27 @@ public class MainActivity extends ActionBarActivity {
 
         musicCursor.close();
     }
+
+
+    @Override
+    protected void onStart( ){
+        super.onStart();
+        if(playIntent == null ){
+            playIntent = new Intent(this, MusicService.class);
+            bindService(playIntent, musicConnection, Context.BIND_AUTO_CREATE);
+            startService(playIntent);
+        }
+    }
+
+    public void songPicked(View view){
+        musicSrv.setSong(Integer.parseInt(view.getTag().toString()));
+        musicSrv.playSong();
+    }
+    @Override
+    protected void onDestroy() {
+        stopService(playIntent);
+        musicSrv=null;
+        super.onDestroy();
+    }
+
 }
